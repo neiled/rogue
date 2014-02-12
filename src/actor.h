ce63697f7155193b9d553a5e1bcbd6f0d9f52928
Neil Edwards <neil@plasticwater.com>
2014-02-12 08:36:56 -0500

    Create actor.h

---
+++ src/actor.h
@@ -0,0 +1,42 @@
+#ifndef ACTOR_H_
+#define ACTOR_H_
+
+class Level;
+class Tile;
+
+#include <deque>
+#include "commands.h"
+#include "world.h"
+
+class Actor
+{
+  public:
+    Actor();
+    virtual ~Actor();
+    Level* getCurrentLevel();
+    void setCurrentTile(Tile* currentTile);
+    Tile* getCurrentTile();
+    int getX();
+    int getY();
+    void moveLeft();
+    void moveRight();
+    void moveUp();
+    void moveDown();
+    World::Direction direction;
+
+    void pushCommand(Commands::CMD command);
+    Commands::CMD popCommand();
+    bool hasCommands();
+    
+  protected:
+    Tile* _currentTile;
+    Tile* _targetTile;
+
+    deque<Commands::CMD> _commandQueue;
+    deque<Tile*> _travelPath;
+    
+    bool attemptMove(int newX, int newY);
+    Tile* checkCanMove(int newX, int newY);
+    Commands::CMD getCommandFromTiles(Tile* start, Tile* end);
+};
+#endif
