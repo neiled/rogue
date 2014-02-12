00e9751f7c71ecf4e8d1f40948ec490d67c43c95
Neil Edwards <neil@plasticwater.com>
2014-02-12 08:37:25 -0500

    move most to Actor

--- src/player.h
+++ src/player.h
@@ -7,40 +7,16 @@ class Tile;
 #include <deque>
 #include "commands.h"
 #include "world.h"
+#include "actor.h"
 
 
-
-
-class Player
+class Player : Actor
 {
   public:
     Player();
     virtual ~Player();
-    Level* getCurrentLevel();
-    void setCurrentTile(Tile* currentTile);
-    Tile* getCurrentTile();
-    int getX();
-    int getY();
-    void moveLeft();
-    void moveRight();
-    void moveUp();
-    void moveDown();
     void explore();
-    World::Direction direction;
-
-    void pushCommand(Commands::CMD command);
-    Commands::CMD popCommand();
-    bool hasCommands();
     
   private:
-    Tile* _currentTile;
-    Tile* _targetTile;
-
-    deque<Commands::CMD> _commandQueue;
-    deque<Tile*> _travelPath;
-    
-    bool attemptMove(int newX, int newY);
-    Tile* checkCanMove(int newX, int newY);
-    Commands::CMD getCommandFromTiles(Tile* start, Tile* end);
 };
 #endif
