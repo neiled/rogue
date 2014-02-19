ec5d3331a5d2800b8374c3dd251f6eb069e1bceb
Neil Edwards <neil@plasticwater.com>
2014-02-19 16:42:02 -0500

    Update potion.h

--- src/potion.h
+++ src/potion.h
@@ -1,11 +1,14 @@
 #ifndef POTION_H
 #define POTION_H
 #include "item.h"
+#include "actor.h"
+
 class Potion : public Item
 {
 public:
   Potion ();
   virtual ~Potion ();
+  void drink(Actor& actor)
 
 private:
   /* data */
