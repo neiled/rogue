70826140f8aa6e50e396175a0eb73a98c9b22d26
Neil Edwards <neil@plasticwater.com>
2014-03-04 09:53:34 +0100

    add calc_cdf

--- src/item_factory.cc
+++ src/item_factory.cc
@@ -4,7 +4,8 @@
 #include <SDL2/SDL.h>
 
 std::map<Item::ItemType, std::map<Item::ItemSubtype, Item*>> ItemFactory::_prototypes;
-std::map<Item::ItemType, std::map<Item::ItemSubtype, int>> ItemFactory::_weighting;
+std::map<Item::ItemType, std::map<Item::ItemSubtype, int>> ItemFactory::_weightings;
+std::map<Item::ItemType, std::map<Item::ItemSubtype, int>> ItemFactory::_cdf;
 
 Item* ItemFactory::Build()
 {
@@ -27,13 +28,36 @@ Item* ItemFactory::Build(Item::ItemType item_type, int xp_level)
 void ItemFactory::Init()
 {
   ItemFactory::init_potions();
+  ItemFactory::calc_cdf();
+}
+
+void ItemFactory::calc_cdf()
+{
+  for(auto types : _weightings)
+  {
+    int sum = 0;
+    for(auto subtypes : _weightings[types->first])
+    {
+      sum += subtypes->second;
+    }
+    _cdf[types->first][subtypes->first] = sum;
+  }
 }
 
 void ItemFactory::init_potions()
 {
-  ItemFactory::_prototypes[Item::ItemType::POTION][Item::ItemSubtype::POTION_HEALTH] = 
-    new Potion("Small Health Potion", Item::ItemSubtype::POTION_HEALTH, {{Actor::Attribute::HEALTH, 25, 0}});
-  ItemFactory::_weighting[Item::ItemType::POTION][Item::ItemSubtype::POTION_HEALTH] =  7500;
+  ItemFactory::add_item(
+    new Potion("Small Health Potion", Item::ItemSubtype::POTION_HEALTH, {{Actor::Attribute::HEALTH, 25, 0}}),
+    7500);
+  ItemFactory::add_item(
+    new Potion("Large Health Potion", Item::ItemSubtype::POTION_HEALTH, {{Actor::Attribute::HEALTH, 50, 0}}),
+    2500);    
+}
+
+void ItemFactory::add_item(Item* item, int weighting)
+{
+  ItemFactory::_prototypes[item->item_type()][item->item_subtype()] = item;
+  ItemFactory::_weightings[item->item_type()][item->item_subtype()] = weighting;
 }
 
 Potion* ItemFactory::get_potion(Item::ItemSubtype subtype)
