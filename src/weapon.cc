067742c9e27b04118a8fb234409c5ddd2ceb772d
Neil Edwards <neil@plasticwater.com>
2014-03-04 11:58:02 +0100

    max_damage

--- src/weapon.cc
+++ src/weapon.cc
@@ -1,10 +1,21 @@
 #include "weapon.h"
 
-Weapon::Weapon(std::string name, Item::ItemSubtype subtype, std::vector<AttributeModifiers> modifiers) : Item(name, Item::ItemType::WEAPON, subtype, modifiers)
+Weapon::Weapon(std::string name, Item::ItemSubtype subtype, int max_damage, std::vector<AttributeModifiers> modifiers)
+  : _max_damage(max_damage), Item(name, Item::ItemType::WEAPON, subtype, modifiers)
 {
 }
 
 
 Weapon::~Weapon()
 {
+}
+
+int Weapon::max_damage()
+{
+  return _max_damage;
+}
+
+int Weapon::calc_damage()
+{
+  return _max_damage;
 }
