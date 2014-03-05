9f4071047d944798cdd8e288cb87921858c159a1
Neil Edwards <neil@plasticwater.com>
2014-03-05 13:30:03 +0100

    calc monsters name

--- src/monster_factory.cc
+++ src/monster_factory.cc
@@ -8,7 +8,8 @@ Monster* MonsterFactory::Build(Tile& tile)
 {
   auto depth = tile.level().depth();
   auto type = MonsterFactory::calc_monster_type(depth);
-  auto monster = new Monster(tile, type, Monster::MonsterState::Awake, calc_xp_level(depth));
+  auto name = MonsterFactory::get_name(type);
+  auto monster = new Monster(name, tile, type, Monster::MonsterState::Awake, calc_xp_level(depth));
   return monster;
 }
 
@@ -25,4 +26,17 @@ int MonsterFactory::calc_xp_level(int depth)
 {
   //TODO: this needs some better logic
   return depth;
+}
+
+std::string MonsterFactoy::get_name(Monster::MonsterType type)
+{
+  switch(type)
+  {
+    case Monster::MonsterType::Orc:
+      return "Orc";
+    case Monster::MonsterType::Devil:
+      return "Devil";
+    default:
+      return "Unknown type";
+  }
 }
