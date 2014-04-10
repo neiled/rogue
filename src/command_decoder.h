f70fb31dba6c8aa980d73930165a0c2b8cdf870c
Neil Edwards <neil@plasticwater.com>
2014-04-10 07:52:50 -0400

    add method defs

--- src/command_decoder.h
+++ src/command_decoder.h
@@ -2,9 +2,10 @@
 #define COMMAND_DECODER_H
 
 #include <SDL2/SDL.h>
-#include "commands.h"
-#include "player.h"
-#include "game.h"
+#include <vector>
+
+class Item;
+class Game;
 
 class CommandDecoder
 {
@@ -12,5 +13,8 @@ public:
   virtual bool Decode(SDL_Keycode key, Game& game) = 0;
   virtual bool Decode(Uint8 button, Uint8 clicks, Sint32 x, Sint32 y, Game& game) = 0;
 
+protected:
+  Item* get_item(std::vector<Item*> items, SDL_Keycode key);  
+  int get_index(SDL_Keycode);
 };
 #endif
