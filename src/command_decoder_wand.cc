843e5abee28d9068f62124bf51666f2a8268f01a
Neil Edwards <neil@plasticwater.com>
2014-04-10 08:00:52 -0400

    Create command_decoder_wand.cc

---
+++ src/command_decoder_wand.cc
@@ -0,0 +1,49 @@
+#include "command_decoder_wand.h"
+#include "game_types.h"
+#include <SDL2/SDL.h>
+
+CommandDecoderWand::CommandDecoderWand() : _drop_mode(false)
+{
+}
+
+bool CommandDecoderWand::Decode(SDL_Keycode key, Game& game)
+{
+  if(key == SDLK_ESCAPE)
+    game.state(GameState::GAME);
+  else if(key == SDLK_i)
+  {
+      game.state(GameState::MENU_INVENTORY);
+  }
+  else if(key == SDLK_w)
+  {
+    game.state(GameState::GAME);
+  }
+  else
+    return use_chosen_item(key, game);
+
+  return true;
+}
+
+bool CommandDecoderWand::use_chosen_item(SDL_Keycode key, Game& game)
+{
+  auto items = get_items(*game.player(), game.state());
+  auto item = get_item(items, key);
+
+  if(!item)
+    return false;
+
+  game.player()->push_command(Commands::CMD::CMD_USE, item, 200);
+
+  game.state(GameState::GAME);
+  return true;
+}
+
+std::vector<Item*> CommandDecoderWand::get_items(Player& player, GameState state)
+{
+  return player.inventory()->items_of_type(ItemType::WAND);
+}
+
+bool CommandDecoderWand::Decode(Uint8 button, Uint8 clicks, Sint32 x, Sint32 y, Game& game)
+{
+  return false;
+}
