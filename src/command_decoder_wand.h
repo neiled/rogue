f77d7895babb7c009b96510e35e53be9ef3be857
Neil Edwards <neil@plasticwater.com>
2014-04-10 08:01:39 -0400

    Update command_decoder_wand.h

--- src/command_decoder_wand.h
+++ src/command_decoder_wand.h
@@ -17,6 +17,7 @@ public:
 private:
   std::vector<Item*> get_items(Player& player, GameState state);
   Item* get_item(std::vector<Item*> items, SDL_Keycode key);
+  bool use_chosen_item(SDL_Keycode key, Game& game);
 
 };
 #endif
