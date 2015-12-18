d5b5ddfb219e8526811fa686a571e50e0b05cdf6
Neil Edwards <neil@plasticwater.com>
2015-12-18 11:42:12 -0500

    Update FontSprite.cpp

--- src/FontSprite.cpp
+++ src/FontSprite.cpp
@@ -5,15 +5,46 @@
 #include <vector>
 #include "FontSprite.h"
 
+SDL_Texture *FontSprite::generate_texture(Graphics* graphics, Uint16 *message, SDL_Color color, int size)
+{
+	TTF_Font *font = graphics->getFont("./content/unifont-8.0.01.ttf", 32);
+	if (font == nullptr)
+	{
+		auto error = TTF_GetError();
+		throw std::runtime_error(std::string("Failed to load font: ") + error);
+	}
+
+
+
+	//Render the message to an SDL_Surface, as that's what TTF_RenderText_X returns
+	//SDL_Surface *surf = TTF_RenderUTF8_Shaded(font, message.c_str(), color, SDL_Color{0,0,0});
+	//SDL_Surface *surf = TTF_RenderText_Shaded(font, message.c_str(), color, SDL_Color{0,0,0});
+	SDL_Surface *surf = get_surface(font, message, color, SDL_Color{ 0, 0, 0 });
+
+		return generate_texture(graphics, surf, size);
+}
+
 SDL_Texture *FontSprite::generate_texture(Graphics* graphics, std::string message, SDL_Color color, int size)
 {
-    TTF_Font *font = graphics->getFont("./content/unifont-8.0.01.ttf", 16);
-    if (font == nullptr)
-        throw std::runtime_error(std::string("Failed to load font: ") + TTF_GetError());
+	TTF_Font *font = graphics->getFont("./content/unifont-8.0.01.ttf", 32);
+	if (font == nullptr)
+	{
+		auto error = TTF_GetError();
+		throw std::runtime_error(std::string("Failed to load font: ") + error);
+	}
 
-    //Render the message to an SDL_Surface, as that's what TTF_RenderText_X returns
-    SDL_Surface *surf = TTF_RenderUNICODE_Shaded(font, std::string("�"), color, SDL_Color{0,0,0});
-//    SDL_Surface *surf = TTF_RenderUNICODE_Shaded(font, message.c_str(), color, SDL_Color{0,0,0});
+
+
+	//Render the message to an SDL_Surface, as that's what TTF_RenderText_X returns
+	//SDL_Surface *surf = TTF_RenderUTF8_Shaded(font, message.c_str(), color, SDL_Color{0,0,0});
+	//SDL_Surface *surf = TTF_RenderText_Shaded(font, message.c_str(), color, SDL_Color{0,0,0});
+	SDL_Surface *surf = get_surface(font, message, color, SDL_Color{ 0, 0, 0 });
+
+	return generate_texture(graphics, surf, size);
+}
+
+SDL_Texture *FontSprite::generate_texture(Graphics* graphics, SDL_Surface* surf, int size)
+{
     SDL_Texture *texture = SDL_CreateTextureFromSurface(graphics->Renderer, surf);
     SDL_Texture *t = SDL_CreateTexture(graphics->Renderer, SDL_PIXELFORMAT_RGBA8888,SDL_TEXTUREACCESS_TARGET, size, size);
 
@@ -32,5 +63,16 @@ SDL_Texture *FontSprite::generate_texture(Graphics* graphics, std::string messag
     //Clean up unneeded stuff
     SDL_FreeSurface(surf);
 
-    return texture;
+    return t;
+}
+
+
+SDL_Surface *FontSprite::get_surface(TTF_Font* font, Uint16 *message, SDL_Color fg, SDL_Color bg)
+{
+	return TTF_RenderUNICODE_Shaded(font, message, fg, bg);
+}
+
+SDL_Surface *FontSprite::get_surface(TTF_Font* font, std::string message, SDL_Color fg, SDL_Color bg)
+{
+	return TTF_RenderText_Shaded(font, message.c_str(), fg, bg);
 }
