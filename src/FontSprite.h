e104cc79764d2eeb6ff26f3e87dc6385a1450c89
Neil Edwards <neil@plasticwater.com>
2015-12-18 11:42:35 -0500

    Update FontSprite.h

--- src/FontSprite.h
+++ src/FontSprite.h
@@ -12,11 +12,19 @@ class FontSprite : public Sprite
 {
 
 public:
-    FontSprite(Graphics *graphics, std::string symbol, int size, SDL_Color color) : Sprite(
-            graphics, generate_texture(graphics, symbol, color, size), 0, 0, size, size)
+    FontSprite(Graphics *graphics, Uint16 *message, int size, SDL_Color color) : Sprite(
+            graphics, generate_texture(graphics, message, color, size), 0, 0, size, size)
     { }
 
-    SDL_Texture *generate_texture(Graphics *graphics, std::string message, SDL_Color color, int size);
+	FontSprite(Graphics *graphics, std::string message, int size, SDL_Color color) : Sprite(
+		graphics, generate_texture(graphics, message, color, size), 0, 0, size, size)
+	{ }
+
+	SDL_Texture *generate_texture(Graphics *graphics, std::string message, SDL_Color color, int size);
+    SDL_Texture *generate_texture(Graphics *graphics, Uint16 *message, SDL_Color color, int size);
+	SDL_Texture *generate_texture(Graphics *graphics, SDL_Surface* surf, int size);
+	SDL_Surface *get_surface(TTF_Font* font, Uint16 *message, SDL_Color fg, SDL_Color bg);
+	SDL_Surface *get_surface(TTF_Font* font, std::string message, SDL_Color fg, SDL_Color bg);
 };
 
 
