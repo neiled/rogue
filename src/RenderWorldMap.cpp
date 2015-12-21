008c07c87dbb2e047c58d1ae5d3910a6e74c541d
Neil Edwards <neil@plasticwater.com>
2015-12-21 15:56:30 -0500

    Move to using GradientMap

--- src/RenderWorldMap.cpp
+++ src/RenderWorldMap.cpp
@@ -13,6 +13,9 @@ RenderWorldMap::RenderWorldMap(Graphics *graphics) : _graphics(graphics)
 
 void RenderWorldMap::render_map(Renderer &renderer,level_sprites_t tiles, SDL_Rect camera, World& world)
 {
+    ColorGradient heatMapGradient;    // Used to create a nice array of different colors.
+    heatMapGradient.createDefaultHeatMapGradient();
+    
     for (int y = 0; y < world.height(); ++y) {
         for (int x = 0; x < world.width(); ++x) {
             if (y * Renderer::TILE_SIZE/4 < camera.y)
@@ -28,24 +31,30 @@ void RenderWorldMap::render_map(Renderer &renderer,level_sprites_t tiles, SDL_Re
             int alpha = 255;
 //
 //
-            draw_tile(&world, tiles, currentTile, x, y, camera.x, camera.y, alpha);
+            draw_tile(&world, heatMapGradient, tiles, currentTile, x, y, camera.x, camera.y, alpha);
         }
     }
 }
 
-void RenderWorldMap::draw_tile(World* world, level_sprites_t sprites, TileType type, int x, int y,
+void RenderWorldMap::draw_tile(World* world, ColorGradient& heatMapGradient, level_sprites_t sprites, TileType type, int x, int y,
                             int camerax, int cameray, int alpha)
 {
     if(!_tile_sprites[y][x])
     {
-        auto color = get_color(world->tile_min_height(), world->tile_max_height(), world->tile_height(x,y));
+        double height, min, max;
+        height = world->tile_height(x,y);
+        min = world->tile_min_height();
+        max = world->tile_max_height();
+        double new_height = 2*((height-min)/(max-min))-1;        
+        
+        float r,g,b;
+        heatMapGradient.getColorAtValue(new_height, r,g,b);        
+        
+        auto color = new SDL_Color{r,g,b};//get_color(world->tile_min_height(), world->tile_max_height(), world->tile_height(x,y));
         auto symbol = get_symbol(world->tile_min_height(), world->tile_max_height(), world->tile_height(x, y));
         _tile_sprites[y][x] = new FontSprite(_graphics, symbol, Renderer::TILE_SIZE/4, color);
 
     }
-//
-    auto sprite = _tile_sprites[y][x];
-
     sprite->draw(x*Renderer::TILE_SIZE/4,y*Renderer::TILE_SIZE/4, camerax, cameray, alpha);
 }
 
@@ -61,44 +70,31 @@ void RenderWorldMap::init()
 
 }
 
-SDL_Color RenderWorldMap::get_color(double min, double max, double height)
-{
-    double new_height = 2*((height-min)/(max-min))-1;
-
-    if(new_height < -0.25)
-        return SDL_Color{0,0,128}; //deeps
-    else if(new_height < 0)
-        return SDL_Color{0,0,255}; //shallow
-    else if(new_height < 0.0625)
-        return SDL_Color{240,240,64}; //shore
-    else if(new_height < 0.1250)
-        return SDL_Color{32,160,0}; //sand      
-    else if(new_height < 0.3750)
-        return SDL_Color{224,224,0}; //grass
-    else if(new_height < 0.7500)
-        return SDL_Color{128,128,128}; //dirt
-    else if(new_height <= 1)
-        return SDL_Color{255,255,255}; //rock 
+// SDL_Color RenderWorldMap::get_color(double min, double max, double height)
+// {
+//     double new_height = 2*((height-min)/(max-min))-1;
 
 
-//    if(new_height < 0.2)
-//        return SDL_Color{46,67,184};
-//    else if(new_height < 0.4)
-//        return SDL_Color{24, 94, 48};
-//    else if(new_height < 0.5)
-//        return SDL_Color{46, 184, 94};
-//    else if(new_height < 0.6)
-//        return SDL_Color{150, 150, 150};
-//    else if(new_height < 0.7)
-//        return SDL_Color{175, 175, 175};
-//    else if(new_height < 0.8)
-//        return SDL_Color{200, 200, 200};
-//    else if(new_height < 0.9)
-//        return SDL_Color{225, 225, 225};
-//    else
-//        return SDL_Color{255,255,255};
 
-}
+
+// //    if(new_height < 0.2)
+// //        return SDL_Color{46,67,184};
+// //    else if(new_height < 0.4)
+// //        return SDL_Color{24, 94, 48};
+// //    else if(new_height < 0.5)
+// //        return SDL_Color{46, 184, 94};
+// //    else if(new_height < 0.6)
+// //        return SDL_Color{150, 150, 150};
+// //    else if(new_height < 0.7)
+// //        return SDL_Color{175, 175, 175};
+// //    else if(new_height < 0.8)
+// //        return SDL_Color{200, 200, 200};
+// //    else if(new_height < 0.9)
+// //        return SDL_Color{225, 225, 225};
+// //    else
+// //        return SDL_Color{255,255,255};
+
+// }
 
 Uint16* RenderWorldMap::get_symbol(double min, double max, double height)
 {
