53a155620899269aca8dec7a1e3734b2266817e7
Neil Edwards <neil@plasticwater.com>
2017-06-21 19:56:17 -0400

    Move to using startX/Y when generating lightmap

--- src/shadowcasting.cc
+++ src/shadowcasting.cc
@@ -8,12 +8,13 @@ ShadowCasting::~ShadowCasting()
 {
 }
 
+//TODO: This needs to become 'add lights' and will take in the level and generate lights based on player and/or light sources in the level
 std::vector<std::vector<float>>  ShadowCasting::calculateFOV(resistance_map_t resistanceMap, int startx, int starty, float radius) {
   return calculateFOV(resistanceMap, startx, starty, 1.0, 1.0 /radius);
 }
 std::vector<std::vector<float>>  ShadowCasting::calculateFOV(resistance_map_t resistanceMap, int startx, int starty, float force, float decay) {
-    _startx = startx;
-    _starty = starty;
+//    _startx = startx;
+//    _starty = starty;
  
     _width = resistanceMap.size();
     _height = resistanceMap[0].size();
@@ -36,8 +37,8 @@ std::vector<std::vector<float>>  ShadowCasting::calculateFOV(resistance_map_t re
       for (int j = -1; j < 2; j+=2)
       {
         
-        castLight(1, 1.0f, 0.0f, 0, i, j, 0, resistanceMap);
-        castLight(1, 1.0f, 0.0f, i, 0, 0, j, resistanceMap);
+        castLight(1, 1.0f, 0.0f, startx, starty, 0, i, j, 0, resistanceMap);
+        castLight(1, 1.0f, 0.0f, startx, starty, i, 0, 0, j, resistanceMap);
       }
       
     }
@@ -50,8 +51,8 @@ float ShadowCasting::radius(float dx, float dy)
 {
   return sqrt(dx*dx+dy*dy);
 }
- 
-void ShadowCasting::castLight(int row, float start, float end, int xx, int xy, int yx, int yy, resistance_map_t resistance_map) {
+
+void ShadowCasting::castLight(int row, float start, float end, int startx, int starty, int xx, int xy, int yx, int yy, resistance_map_t resistance_map) {
     float newStart = 0.0f;
     if (start < end) {
         return;
@@ -60,24 +61,24 @@ void ShadowCasting::castLight(int row, float start, float end, int xx, int xy, i
     for (int distance = row; distance <= _radius && !blocked; distance++) {
         int deltaY = -distance;
         for (int deltaX = -distance; deltaX <= 0; deltaX++) {
-            int currentX = _startx + deltaX * xx + deltaY * xy;
-            int currentY = _starty + deltaX * yx + deltaY * yy;
+            int currentX = startx + deltaX * xx + deltaY * xy;
+            int currentY = starty + deltaX * yx + deltaY * yy;
             float leftSlope = (deltaX - 0.5f) / (deltaY + 0.5f);
             float rightSlope = (deltaX + 0.5f) / (deltaY - 0.5f);
- 
+
             if (!(currentX >= 0 && currentY >= 0 && currentX < _width && currentY < _height) || start < rightSlope) {
                 continue;
             } else if (end > leftSlope) {
                 break;
             }
- 
+
             //check if it's within the lightable area and light if needed
             if (radius(deltaX, deltaY) <= _radius) {
                 float bright = (float) (1 - (_decay * radius(deltaX, deltaY) / _force));
 
                 _lightMap[currentY][currentX] = bright;
             }
- 
+
             if (blocked) { //previous cell was a blocking one
                 if (blockingCell(resistance_map[currentY][currentX])) {//hit a wall
                     newStart = rightSlope;
@@ -89,7 +90,7 @@ void ShadowCasting::castLight(int row, float start, float end, int xx, int xy, i
             } else {
                 if (blockingCell(resistance_map[currentY][currentX]) && distance < _radius) {//hit a wall within sight line
                     blocked = true;
-                    castLight(distance + 1, start, leftSlope, xx, xy, yx, yy, resistance_map);
+                    castLight(distance + 1, start, leftSlope, startx, starty, xx, xy, yx, yy, resistance_map);
                     newStart = rightSlope;
                 }
             }
--- src/shadowcasting.h
+++ src/shadowcasting.h
@@ -11,25 +11,27 @@ typedef std::array<std::array<Tile*, Level::LEVEL_WIDTH>, Level::LEVEL_HEIGHT> r
 class ShadowCasting
 {
 public:
-  ShadowCasting ();
-  virtual ~ShadowCasting ();
-  std::vector<std::vector<float>> calculateFOV(resistance_map_t resistanceMap, int startx, int starty, float force, float decay);
-  std::vector<std::vector<float>> calculateFOV(resistance_map_t resistanceMap, int startx, int starty, float radius);
-  void castLight(int row, float start, float end, int xx, int xy, int yx, int yy, resistance_map_t resistance_map);
-  float radius(float dx, float dy);
-  bool blockingCell(Tile* tile);
+    ShadowCasting ();
+    virtual ~ShadowCasting ();
+    std::vector<std::vector<float>> calculateFOV(resistance_map_t resistanceMap, int startx, int starty, float force, float decay);
+    std::vector<std::vector<float>> calculateFOV(resistance_map_t resistanceMap, int startx, int starty, float radius);
+    //  void castLight(int row, float start, float end, int xx, int xy, int yx, int yy, resistance_map_t resistance_map);
+    float radius(float dx, float dy);
+    bool blockingCell(Tile* tile);
 
 private:
-  int _startx;
-  int _starty;
-  float _radius;
-  float _force;
-  float _decay;
-  //std::array<std::array<std::shared_ptr<Tile>, Level::LEVEL_WIDTH>, Level::LEVEL_HEIGHT > _resistanceMap;
-  //std::array<std::array<Tile&, Level::LEVEL_WIDTH>, Level::LEVEL_HEIGHT> _resistanceMap;
-  int _width;
-  int _height;
-  std::vector<std::vector<float> > _lightMap;
+//  int _startx;
+//  int _starty;
+    float _radius;
+    float _force;
+    float _decay;
+    //std::array<std::array<std::shared_ptr<Tile>, Level::LEVEL_WIDTH>, Level::LEVEL_HEIGHT > _resistanceMap;
+    //std::array<std::array<Tile&, Level::LEVEL_WIDTH>, Level::LEVEL_HEIGHT> _resistanceMap;
+    int _width;
+    int _height;
+    std::vector<std::vector<float> > _lightMap;
 
+    void castLight(int row, float start, float end, int startx, int starty, int xx, int xy, int yx, int yy,
+                   resistance_map_t resistance_map);
 };
 #endif
